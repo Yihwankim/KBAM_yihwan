bc3b15b92580ab54fb564bf12bb0694df71ba7e9
check <kimleehwan3@gmail.com>
2022-04-22 17:12:40 +0900

    220421

--- spread_checking_YH_v0420_check.py
+++ spread_checking_YH_v0420_check.py
@@ -44,15 +44,17 @@ def start_code():
 # 16시 00에 종료
 def exit_code():
     print("장 마감으로 인한 스프레드 점검 종료")
-    bot.sendMessage(chat_id=chat_id, text="@@ LP 호가 제출 의무 종료 @@)")
+    bot.sendMessage(chat_id=chat_id, text="@@ LP 호가 제출 의무 종료 @@")
     sys.exit()  # 프로그램 종료
 
 
-########################################################################################################################
-# Start code
+# start <-- 처음 한번만 실행
 start_code()
 
-schedule.every().day.at("16:00").do(exit_code)  # 4시에 종료
+########################################################################################################################
+# Start code
+
+schedule.every().day.at("15:20").do(exit_code)  # 15시 20분에 종료
 
 while True:
     schedule.run_pending()
@@ -61,31 +63,33 @@ while True:
 
     for i in range(len(etf_list)):
         if etf_list['check'][i] == "Y":
-            notification("스프레드 점검 필요")
-
             code_y = etf_list['code'][i]
             name_y = etf_list['name'][i]
             ask_y = etf_list['ask1'][i]
             bid_y = etf_list['bid1'][i]
             gap_y = etf_list['gap'][i]
-            spread_y = round(etf_list['spread'][i], 4)
-            criterion_y = etf_list['criterion'][i]
+            spread_y = round(etf_list['spread'][i], 4) * 100
+            criterion_y = etf_list['criterion'][i] * 100
 
-            now_time = time.strftime('%H:%M:%S')
-            warr = "* " + name_y + "(A" + str(code_y) + ")" + "  " + now_time
-            spread_value = "매도: " + str(ask_y) + " " + "매수: " + str(bid_y) + " " + "차이: " + str(gap_y)
-            spread_mgt = "- 관리 스프레드: " + str(criterion_y)
-            spread_now = "- 현재 스프레드: " + str(spread_y)
+            now_time = "[" + time.strftime('%H:%M:%S') + "]" + "\n"
+            warr = name_y + "(A" + str(code_y) + ")" + "\n"
+            spread_value = "매도: " + str(ask_y) + " " + "매수: " + str(bid_y) + " " + "차이: " + str(gap_y) + "\n"
+            spread_mgt = "- 관리 스프레드: " + str(criterion_y) + "%" + "\n"
+            spread_now = "- 현재 스프레드: " + str(spread_y) + "%"
+
+            spread_send = now_time + warr + spread_value + spread_mgt + spread_now
 
             print("스프레드 위반")
-            print(warr)
-            print(spread_value)
+            print(spread_send)
 
-            notification(warr)
-            notification(spread_value)
-            notification(spread_mgt)
-            notification(spread_now)
+            # notification(warr)
+            # notification(spread_value)
+            # notification(spread_mgt)
+            # notification(spread_now)
+            notification(spread_send)
+            # notification("------------------------------------------")
 
     print("------------------------------------------")
 
-    time.sleep(60)
+    time.sleep(180)
+    print(now_time)
